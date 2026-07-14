d8afe2edd5ce9b19c19248079fa0f73e1d65bc64
naxida-ima <3692725705@qq.com>
2026-07-14 20:25:01 +0800

    chore(obf): 用派生 key 重生成 41 个混淆常量

--- app/src/main/java/com/example/clock/Obf.kt
+++ app/src/main/java/com/example/clock/Obf.kt
@@ -23,47 +23,48 @@ object Obf {
 
     // ===== SharedPreferences 文件名 & keys =====
     // ===== SharedPreferences 文件名 & keys =====
-    val PREF_NAME          get() = d("OTY1OTE=")  // clock
-    val KEY_SEL_SERVER          get() = d("KT82PzkuPz4FKT8oLD8o")  // selected_server
-    val KEY_CUSTOM_SVRS          get() = d("OS8pLjU3BSk/KCw/KCk=")  // custom_servers
-    val KEY_SEL_HISTORY          get() = d("KT82PzkuMzU0BTIzKS41KCM=")  // selection_history
-    val KEY_ONLINE_ON          get() = d("NTQ2MzQ/BTU0")  // online_on
-    val KEY_REMIND_ON          get() = d("KD83MzQ+BTU0")  // remind_on
-    val KEY_NEXT_ENTRY_ON          get() = d("ND8iLgU/NC4oIwU1NA==")  // next_entry_on
-    val KEY_DEVICE_UUID          get() = d("Pj8sMzk/BS8vMz4=")  // device_uuid
-    val EMPTY_ARR          get() = d("AQc=")  // []
+    val PREF_NAME          get() = d("ZWppZW0=")  // clock
+    val KEY_SEL_SERVER          get() = d("dWNqY2VyY2JZdWN0cGN0")  // selected_server
+    val KEY_CUSTOM_SVRS          get() = d("ZXN1cmlrWXVjdHBjdHU=")  // custom_servers
+    val KEY_SEL_HISTORY          get() = d("dWNqY2Vyb2loWW5vdXJpdH8=")  // selection_history
+    val KEY_ONLINE_ON          get() = d("aWhqb2hjWWlo")  // online_on
+    val KEY_REMIND_ON          get() = d("dGNrb2hiWWlo")  // remind_on
+    val KEY_NEXT_ENTRY_ON          get() = d("aGN+clljaHJ0f1lpaA==")  // next_entry_on
+    val KEY_DEVICE_UUID          get() = d("YmNwb2VjWXNzb2I=")  // device_uuid
+    val EMPTY_ARR          get() = d("XVs=")  // []
     // ===== URL 路径片段 =====
-    val URL_HEARTBEAT          get() = d("dTI/OyguOD87LmUzPmc=")  // /heartbeat?id=
-    val URL_NEXT_ENTRY          get() = d("dTQ/Ii4/NC4oIw==")  // /nextentry
+    val URL_HEARTBEAT          get() = d("KW5jZ3RyZGNncjlvYjs=")  // /heartbeat?id=
+    val URL_NEXT_ENTRY          get() = d("KWhjfnJjaHJ0fw==")  // /nextentry
     // ===== Toast / UI 文字 =====
-    val TOAST_ONLINE_ON          get() = d("v/TEvM3sv8byveDlvuDgvM/qtebAv+ba")  // 实时在线人数：开
-    val TOAST_ONLINE_OFF          get() = d("v/TEvM3sv8byveDlvuDgvM/qtebAv9/p")  // 实时在线人数：关
-    val TOAST_REMIND_ON          get() = d("vNXKs9zItebAv+ba")  // 提醒：开
-    val TOAST_REMIND_OFF          get() = d("vNXKs9zItebAv9/p")  // 提醒：关
-    val TOAST_SELECTED          get() = d("v+3os9rTvNHztebA")  // 已选择：
-    val TOAST_NEXT_ON          get() = d("vuLRvPb7suXBv9//vM3ss83utebAv+ba")  // 下次进入时间：开
-    val TOAST_NEXT_OFF          get() = d("vuLRvPb7suXBv9//vM3ss83utebAv9/p")  // 下次进入时间：关
-    val SYNC_ING          get() = d("v8rWvPf/vuL3uNr8")  // 同步中…
-    val SYNC_OK          get() = d("v+3ovPr7v93c")  // 已校准
-    val SYNC_FAIL          get() = d("vMbwvPr7v93cepjterzG9rzG4LzN7LPN7g==")  // 未校准 · 本机时间
-    val ONLINE_DASH          get() = d("v8byveDlend3")  // 在线 --
-    val ONLINE_ERR          get() = d("v8byveDlerLU7b/VzL/+67Lu/w==")  // 在线 获取失败
-    val ONLINE_FMT          get() = d("v8byveDlen8+er7g4A==")  // 在线 %d 人
-    val NEXT_ENTRY_FMT          get() = d("vuLRvPb7suXBv9//en8p")  // 下次进入 %s
-    val DLG_ADD_TITLE          get() = d("vO3hv9D6st3wv/TAvuPTvMbXv9D7v8Py")  // 添加自定义服务器
-    val DLG_ADD_HINT          get() = d("vuTRv/zYejQuKnQ/Ijs3KjY/dDk1Nw==")  // 例如 ntp.example.com
-    val DLG_ADD_MSG          get() = d("v/vxv9zDehQOCnq8xte/0Pu/w/K/xuq/x9q15tIPHgp6vfH1v9X5emtoabXm0w==")  // 填写 NTP 服务器地址（UDP 端口 123）
-    val DLG_ADD_OK          get() = d("vO3hv9D6")  // 添加
-    val DLG_ADD_CANCEL          get() = d("v9XMvOzS")  // 取消
-    val TOAST_EXISTS          get() = d("v+3ov/fCv8by")  // 已存在
-    val TOAST_NO_DEL          get() = d("s+HCsvT+vMbXv9D7v8PyvuLXv9X1v9L6s8P+")  // 默认服务器不可删除
-    val DLG_DEL_TITLE          get() = d("v9L6s8P+vMbXv9D7v8Py")  // 删除服务器
-    val DLG_DEL_MSG          get() = d("vfv0v/TAv9L6s8P+en8perXmxQ==")  // 确定删除 %s ？
-    val DLG_DEL_OK          get() = d("v9L6s8P+")  // 删除
-    val LATENCY_ING          get() = d("vO/Rs9rFvuL3uNr8")  // 测速中…
-    val LATENCY_FMT          get() = d("v+HssuXFen8+ejcp")  // 延迟 %d ms
-    val LATENCY_TIMEOUT          get() = d("v+HssuXFerLs37zN7A==")  // 延迟 超时
-    val LABEL_SETTINGS          get() = d("vMbXv9D7v8PysvTkvef0")  // 服务器设置
-    val LABEL_CUSTOM          get() = d("tebSst3wv/TAvuPTtebT")  // （自定义）
-    val LABEL_SELECTED          get() = d("uMbJen8p")  // ✓ %s
+    val TOAST_ONLINE_ON          get() = d("46iY4JGw45qu4by54ry84JO26bqc47qG")  // 实时在线人数：开
+    val TOAST_ONLINE_OFF          get() = d("46iY4JGw45qu4by54ry84JO26bqc44O1")  // 实时在线人数：关
+    val TOAST_REMIND_ON          get() = d("4ImW74CU6bqc47qG")  // 提醒：开
+    val TOAST_REMIND_OFF          get() = d("4ImW74CU6bqc44O1")  // 提醒：关
+    val TOAST_SELECTED          get() = d("47G074aP4I2v6bqc")  // 已选择：
+    val TOAST_NEXT_ON          get() = d("4r6N4Kqn7rmd44Oj4JGw75Gy6bqc47qG")  // 下次进入时间：开
+    val TOAST_NEXT_OFF          get() = d("4r6N4Kqn7rmd44Oj4JGw75Gy6bqc44O1")  // 下次进入时间：关
+    val SYNC_ING          get() = d("45aK4Kuj4r6r5Iag")  // 同步中…
+    val SYNC_OK          get() = d("47G04Kan44GA")  // 已校准
+    val SYNC_FAIL          get() = d("4Jqs4Kan44GAJsSxJuCaquCavOCRsO+Rsg==")  // 未校准 · 本机时间
+    val ONLINE_DASH          get() = d("45qu4by5Jisr")  // 在线 --
+    val ONLINE_ERR          get() = d("45qu4by5Ju6IseOJkOOit+6yow==")  // 在线 获取失败
+    val ONLINE_FMT          get() = d("45qu4by5JiNiJuK8vA==")  // 在线 %d 人
+    val NEXT_ENTRY_FMT          get() = d("4r6N4Kqn7rmd44OjJiN1")  // 下次进入 %s
+    val DLG_ADD_TITLE          get() = d("4LG944ym7oGs46ic4r+P4JqL44yn45+u")  // 添加自定义服务器
+    val DLG_ADD_HINT          get() = d("4riN46CEJmhydihjfmdrdmpjKGVpaw==")  // 例如 ntp.example.com
+    val DLG_ADD_MSG          get() = d("46et44CfJkhSVibgmovjjKfjn67jmrbjm4bpuo5TQlYm4a2p44mlJjc0Nem6jw==")  // 填写 NTP 服务器地址（UDP 端口 123）
+    val DLG_ADD_OK          get() = d("4LG944ym")  // 添加
+    val DLG_ADD_CANCEL          get() = d("44mQ4LCO")  // 取消
+    val TOAST_EXISTS          get() = d("47G046ue45qu")  // 已存在
+    val TOAST_NO_DEL          get() = d("772e7qii4JqL44yn45+u4r6L44mp446m75+i")  // 默认服务器不可删除
+    val DLG_DEL_TITLE          get() = d("446m75+i4JqL44yn45+u")  // 删除服务器
+    val DLG_DEL_MSG          get() = d("4aeo46ic446m75+iJiN1Jum6mQ==")  // 确定删除 %s ？
+    val DLG_DEL_OK          get() = d("446m75+i")  // 删除
+    val LATENCY_ING          get() = d("4LON74aZ4r6r5Iag")  // 测速中…
+    val LATENCY_FMT          get() = d("472w7rmZJiNiJmt1")  // 延迟 %d ms
+    val LATENCY_TIMEOUT          get() = d("472w7rmZJu6wg+CRsA==")  // 延迟 超时
+    val LABEL_SETTINGS          get() = d("4JqL44yn45+u7qi44buo")  // 服务器设置
+    val LABEL_CUSTOM          get() = d("6bqO7oGs46ic4r+P6bqP")  // （自定义）
+    val LABEL_SELECTED          get() = d("5JqVJiN1")  // ✓ %s
 }
+
